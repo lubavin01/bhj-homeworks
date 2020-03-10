4aca09e82ab7ec677c0026d17c1d427eccf671c0
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-03-10 15:59:48 +0300

    popup homework

---
+++ element-search/popups/task.js
@@ -0,0 +1,24 @@
+(function() {
+
+    const modal = document.getElementById('modal_main');
+    const modalSuccess = document.getElementById('modal_success');
+    modal.classList.add(`modal_active`);
+
+    onClickButton = function (event) {
+
+        if (!event.target.matches('.modal__close')) {
+            return;
+        }
+
+        const popup = event.target.closest('div.modal');
+        popup.classList.remove(`modal_active`);
+
+        if (event.target.matches('.show-success')) {
+            modalSuccess.classList.add(`modal_active`);
+        }
+    }
+
+    modal.onclick = onClickButton;
+    modalSuccess.onclick = onClickButton;
+
+})()
