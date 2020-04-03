574ccac087ee0b534556c89ab63e1ab7d39c716e
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-04-03 21:47:34 +0300

    interest difficult hw update

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,13 +1,40 @@
 const interests = document.querySelectorAll('.interest__check');
 const main = document.querySelector('.interests_main');
 
-main.addEventListener('click', (e) => {
 
-    if (e.target.tagName !== 'INPUT') {return};
+const goUp = (eTarget, goDown = true) => {
+    const rootUl = eTarget.closest('ul.interests');
+    if (rootUl) {
+        const rootNode = rootUl.previousElementSibling.firstElementChild;
 
-    console.log(e.target);
+        const checked = rootUl.querySelectorAll('.interest__check:checked');
+        const inputs = rootUl.querySelectorAll('.interest__check');
+
+        rootNode.checked = checked.length === inputs.length;
+        if (!rootNode.checked && checked.length) {
+            rootNode.indeterminate = true;
+        }
+        else {
+            rootNode.indeterminate = false;
+        }
+
+        goUp(rootNode, false);
+    }
+
+
+
+    if (goDown) {
+        const rootLi = eTarget.closest('ul>li');
+        const childInputs = rootLi.querySelectorAll('ul .interest__check');
+        [...childInputs].forEach(element => {
+            element.checked = eTarget.checked;
+        });
+    }
+
+
     
+}
 
-    
 
-});
+
+main.addEventListener('click', (e) => goUp(e.target));
