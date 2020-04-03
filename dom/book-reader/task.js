8a101afab92da49476136af2c044c098733ff234
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-04-03 22:27:41 +0300

    correction to regular functions

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -5,11 +5,22 @@ const fontControls = document.querySelector('.book__control_font-size');
 const colorControls = document.querySelector('.book__control_color');
 const bgControls = document.querySelector('.book__control_background');
 
-function toggleNewClass(e) {
+function toggleNewClass(e, data) {
     e.preventDefault();
+    const {checkClass, allClasses, template, attr, activeClass} = data;
     if (!e.target.classList.contains(checkClass)) {
         return;
     }
+
+    const block = e.target.closest('div');
+    const buttons = block.querySelectorAll('a');
+    [...buttons].forEach(i => {
+        if (i === e.target) {
+            i.classList.add(activeClass);
+        } else {
+            i.classList.remove(activeClass);
+        }
+    });   
 
     allClasses.forEach(item => book.classList.remove(item));
     if (e.target.dataset[attr]) {
@@ -18,64 +29,65 @@ function toggleNewClass(e) {
     }
 }
 
-function toggleFont() {
+// Не работает
+// function toggleFont() {
 
-    let allClasses = ['book_fs-big', 'book_fs-small'];
-    let template = 'book_fs-';
-    let checkClass = 'font-size';
-    let attr = 'size';
-    let counter = 0;
+//     let allClasses = ['book_fs-big', 'book_fs-small'];
+//     let template = 'book_fs-';
+//     let checkClass = 'font-size';
+//     let attr = 'size';
+//     let counter = 0;
 
-    return (e) => { 
-        let counter = 0;
-        return toggleNewClass(e)};
-}
+//     const f = (e) => { 
+//         let counter = 0;
+//         return toggleNewClass(e)}
 
-function toggleColor() {
-    let allClasses = ['book_color-gray', 'book_color-whitesmoke'];
-    let template = 'book_color-';
-    let checkClass = 'color';
-    let attr = 'color';
+//     return f;
+// }
 
-    return function (e) { return toggleNewClass(e) };
-}
+// function toggleColor() {
+//     let allClasses = ['book_color-gray', 'book_color-whitesmoke'];
+//     let template = 'book_color-';
+//     let checkClass = 'color';
+//     let attr = 'color';
 
-fontControls.addEventListener('click', toggleFont());
+//     return function (e) { return toggleNewClass(e) };
+// }
+
+fontControls.addEventListener('click', (e) => {
+    const data = {
+        allClasses: ['book_fs-big', 'book_fs-small'],
+        template: 'book_fs-',
+        checkClass: 'font-size',
+        attr: 'size',
+        activeClass: 'font-size_active',
+    }
+
+    toggleNewClass(e, data);
+});
 //fontControls.onClick = toggleFont();
 
-colorControls.addEventListener('click', toggleColor());
+colorControls.addEventListener('click', (e) => {
+    const data = {
+        allClasses: ['book_color-gray', 'book_color-whitesmoke'],
+        template: 'book_color-',
+        checkClass: 'color',
+        attr: 'color',
+        activeClass: 'color_active',
+    }
+
+    toggleNewClass(e, data);
+});
 //colorControls.onClick = toggleFont();
 
+bgControls.addEventListener('click', (e) => {
+    const data = {
+        allClasses: ['book_bg-gray', 'book_bg-black'],
+        template: 'book_bg-',
+        checkClass: 'color',
+        attr: 'color',
+        activeClass: 'color_active',
+    }
 
-
-
-
-
-// fontControls.addEventListener('click', (e) => {
-//     const allClasses = ['book_fs-big', 'book_fs-small'];
-//     const template = 'book_fs-';
-//     const checkClass = 'font-size';
-//     const attr = 'size';
-
-//     const toggle = (e) => {return toggleNewClass(e)}
-//     return toggle;
-// });
-
-// colorControls.addEventListener('click', (e) => {
-//     const allClasses = ['book_color-gray', 'book_color-whitesmoke'];
-//     const template = 'book_color-';
-//     const checkClass = 'color';
-//     const attr = 'color';
-
-//     toggleNewClass(e);
-// });
-
-// bgControls.addEventListener('click', (e)=> {
-//     const allClasses = ['book_bg-gray', 'book_bg-white'];
-//     const template = 'bbook_bg-';
-//     const checkClass = 'color';
-//     const attr = 'color';
-
-//     toggleNewClass(e);    
-// });
-
+    toggleNewClass(e, data);
+});
